0eb670d99d80bb81f2ef1388623e0e0fba271194
CXVMNER <54186131+CXVMNER@users.noreply.github.com>
2025-10-13 12:41:06 +0200

    Added walking up and down the stairs.

--- scripts/fpp_controller.gd
+++ scripts/fpp_controller.gd
@@ -21,6 +21,7 @@ const JUMP_VELOCITY := 4.0
 const SENSITIVITY := 0.01
 const BACKWARD_SPEED := 0.8  # 80% of normal speed when moving backwards
 
+
 const AIR_CONTROL_FACTOR := 0.3  # Controls how much air movement is allowed (0 = no control, 1 = full control)
 var stored_horizontal_velocity := Vector3.ZERO  # To store the velocity at the moment of jumping
 
@@ -28,6 +29,11 @@ var stored_horizontal_velocity := Vector3.ZERO  # To store the velocity at the m
 const BOB_FREQ := 2.0
 const BOB_AMP := 0.06
 var t_bob := 0.0
+
+const MAX_STEP_HEIGHT := 0.25
+var _snapped_to_stairs_last_frame := false
+var _last_frame_was_on_floor = -INF
+
 
 var is_crouching := false
 @export_range(5, 10, 0.1) var CROUCHING_SPEED : float = 7.0 # Animation speed
@@ -98,6 +104,8 @@ func _unhandled_input(event):
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _physics_process(delta):
+	if is_on_floor(): _last_frame_was_on_floor = Engine.get_physics_frames()
+	
 	# Handle gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -136,7 +144,7 @@ func _physics_process(delta):
 	var direction = (CameraController.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
 	# Modify movement based on whether the player is on the floor or in the air
-	if is_on_floor():
+	if is_on_floor() or _snapped_to_stairs_last_frame:
 		# Normal movement control on the ground
 		if direction:
 			# Apply speed reduction when "move_backward" is pressed
@@ -161,8 +169,13 @@ func _physics_process(delta):
 	# Head bob (only when on the ground)
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = _headbob(t_bob)
-
-	move_and_slide()
+	
+	if not _snap_up_stairs_check(delta):
+		# Because _snap_up_stairs_check moves the body manually, don't call move_and_slide
+		# This should be fine since we ensure with the body_test_motion that it doesn't 
+		# collide with anything except the stairs it's moving up to.
+		move_and_slide()
+		_snap_down_to_stairs_check()
 
 func _fire():
 	var now := Time.get_ticks_msec()/1000.0
@@ -186,6 +199,50 @@ func _process(_delta):
 	if Input.is_action_just_released("sprint"):
 		speed = WALK_SPEED
 		staminaRegenTimer.start()
+
+func _snap_down_to_stairs_check() -> void:
+	var did_snap := false
+	# Since it is called after move_and_slide, _last_frame_was_on_floor should still be current frame number.
+	# After move_and_slide off top of stairs, on floor should then be false. Update raycast incase it's not already.
+	%StairsBelowRayCast3D.force_raycast_update()
+	var floor_below : bool = %StairsBelowRayCast3D.is_colliding() and not is_surface_too_steep(%StairsBelowRayCast3D.get_collision_normal())
+	var was_on_floor_last_frame = Engine.get_physics_frames() - _last_frame_was_on_floor
+	if not is_on_floor() and velocity.y <= 0  and (was_on_floor_last_frame or _snapped_to_stairs_last_frame) and floor_below:
+		var body_test_result = PhysicsTestMotionResult3D.new()
+		if _run_body_test_motion(self.global_transform, Vector3(0, -MAX_STEP_HEIGHT, 0), body_test_result):
+			var translate_y = body_test_result.get_travel().y
+			self.position.y += translate_y
+			apply_floor_snap()
+			did_snap = true
+	_snapped_to_stairs_last_frame = did_snap
+	
+func _snap_up_stairs_check(delta) -> bool:
+	if not is_on_floor() and not _snapped_to_stairs_last_frame: return false
+	# Don't snap stairs if trying to jump, also no need to check for stairs ahead if not moving
+	if self.velocity.y > 0 or (self.velocity * Vector3(1,0,1)).length() == 0: return false
+	var expected_move_motion = self.velocity * Vector3(1,0,1) * delta
+	var step_pos_with_clearance = self.global_transform.translated(expected_move_motion + Vector3(0, MAX_STEP_HEIGHT * 2, 0))
+	# Run a body_test_motion slightly above the pos we expect to move to, towards the floor.
+	#  We give some clearance above to ensure there's ample room for the player.
+	#  If it hits a step <= MAX_STEP_HEIGHT, we can teleport the player on top of the step
+	#  along with their intended motion forward.
+	var down_check_result = KinematicCollision3D.new()
+	if (self.test_move(step_pos_with_clearance, Vector3(0,-MAX_STEP_HEIGHT*2,0), down_check_result)
+	and (down_check_result.get_collider().is_class("StaticBody3D") or down_check_result.get_collider().is_class("CSGShape3D"))):
+		var step_height = ((step_pos_with_clearance.origin + down_check_result.get_travel()) - self.global_position).y
+		# Note I put the step_height <= 0.01 in just because I noticed it prevented some physics glitchiness
+		# 0.02 was found with trial and error. Too much and sometimes get stuck on a stair. Too little and can jitter if running into a ceiling.
+		# The normal character controller (both jolt & default) seems to be able to handled steps up of 0.1 anyway
+		if step_height > MAX_STEP_HEIGHT or step_height <= 0.01 or (down_check_result.get_position() - self.global_position).y > MAX_STEP_HEIGHT: return false
+		%StairsAheadRayCast3D.global_position = down_check_result.get_position() + Vector3(0,MAX_STEP_HEIGHT,0) + expected_move_motion.normalized() * 0.1
+		%StairsAheadRayCast3D.force_raycast_update()
+		if %StairsAheadRayCast3D.is_colliding() and not is_surface_too_steep(%StairsAheadRayCast3D.get_collision_normal()):
+			# _save_camera_pos_for_smoothing()
+			self.global_position = step_pos_with_clearance.origin + down_check_result.get_travel()
+			apply_floor_snap()
+			_snapped_to_stairs_last_frame = true
+			return true
+	return false
 
 func gainAmmo(qty):
 	hud.ammo += qty
@@ -265,6 +322,16 @@ func _on_hitbox_body_entered(body):
 
 func _on_stamina_regen_timeout():
 	regenStamina = true
+
+func is_surface_too_steep(normal : Vector3) -> bool:
+	return normal.angle_to(Vector3.UP) > self.floor_max_angle
+
+func _run_body_test_motion(from : Transform3D, motion : Vector3, result = null) -> bool:
+	if not result: result = PhysicsTestMotionResult3D.new()
+	var params = PhysicsTestMotionParameters3D.new()
+	params.from = from
+	params.motion = motion
+	return PhysicsServer3D.body_test_motion(self.get_rid(), params, result)
 
 # func _on_fallzone_body_entered(body):
 # 	get_tree().change_scene_to_file("res://scenes/level_01.tscn")
